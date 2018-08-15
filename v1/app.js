5da49690312498411871e233607b8bd2f00bc48c
Waraphon Roonnapai <eepw0211@gmail.com>
2018-08-15 21:54:48 +0700

    Create new campground

--- v1/app.js
+++ v1/app.js
@@ -2,7 +2,25 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
+
+const campgrounds = [{
+  name: 'camp1',
+  image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f3c079aeedbdb0_340.jpg',
+},
+{
+  name: 'camp2',
+  image: 'https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104496f3c079aeedbdb0_340.jpg',
+},
+{
+  name: 'camp3',
+  image: 'https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg',
+},
+];
 
 app.get('/', (req, res) => {
   // res.send("Landing Page..."
@@ -10,23 +28,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [{
-    name: 'camp1',
-    image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f3c079aeedbdb0_340.jpg',
-  },
-  {
-    name: 'camp2',
-    image: 'https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104496f3c079aeedbdb0_340.jpg',
-  },
-  {
-    name: 'camp3',
-    image: 'https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg',
-  },
-  ];
-
   res.render('campgrounds', {
     campgrounds,
   });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/campgrounds', (req, res) => {
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCampground = { name, image };
+  campgrounds.push(newCampground);
+
+  res.redirect('/campgrounds');
+  // Get data from form to arry => to get
 });
 
 app.listen(3000, () => {
