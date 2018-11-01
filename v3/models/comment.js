c95248eacb4ff88a3c06b027b6894624fd97c84b
Waraphon Roonnapai <eepw0211@gmail.com>
2018-11-01 10:38:11 +0000

    add new branch for final project

--- v3/models/comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const CommentSchema = new mongoose.Schema({
-    text:String,
-    author:String
-});
-    
-const commentModel = mongoose.model("Comment",CommentSchema);    
-module.exports = commentModel;
